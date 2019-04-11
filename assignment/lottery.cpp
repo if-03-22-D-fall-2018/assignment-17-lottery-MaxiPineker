20824c4cd203cd970c330d9e0d1165b091e76078
MaxiPineker <maxi.pineker@gmail.com>
2019-04-11 11:35:51 +0200

    first 2 tests are runnning

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -10,3 +10,26 @@
  * Implementation of a library for analyzing lottery tips.
  *-----------------------------------------------------------------------------
 */
+#include <stdio.h>
+#include "lottery.h"
+#define UUID_LEN 40
+#define MAX_TIP_LEN 17
+#define MAX_LINE_LEN (UUID_LEN + 1 + MAX_TIP_LEN +1)
+
+
+bool init_lottery(const char* csv_file, char csv_seperator){
+  FILE* stream = fopen(csv_file, "r");
+  return stream != 0;
+}
+bool get_tip(int tip_number, int tip[TIP_SIZE]){
+ return false;
+}
+bool set_drawing(int drawing_numbers[TIP_SIZE]){
+ return false;
+}
+int get_tip_result(int tip_number){
+ return 0;
+}
+int get_right_tips_count(int right_digits_count){
+ return 0;
+}
