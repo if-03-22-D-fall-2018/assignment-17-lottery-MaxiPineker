b3fadddb72c278ad675d67700181fa55aabf9019
Maxi Pineker <maxi.pineker@gmail.com>
2019-05-01 23:23:19 +0200

    changed things and did get_tip_result

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -21,31 +21,27 @@
 
 FILE *_stream;
 char _seperator;
-int _line_number = 0;
 int _last_drawing[TIP_SIZE];
 
-int get_line_numbers();
+bool is_empty(int drawing_numbers[]);
 bool contains_digit(int tip[TIP_SIZE], int digit);
 
 bool init_lottery(const char *csv_file, char csv_separator)
 {
-    _stream = fopen(csv_file, "r");
-    if (_stream == 0)
-    {
-        return false;
-    }
-    _seperator = csv_separator;
-    _line_number = get_line_numbers();
-    for (int i = 0; i < TIP_SIZE; i++)
-    {
-        _last_drawing[i] = 0;
-    }
-    return true;
+  _stream = fopen(csv_file, "r");
+  if (_stream == 0) {
+    return false;
+  }
+  _seperator = csv_separator;
+  for (int i = 0; i < TIP_SIZE; i++) {
+    _last_drawing[i] = 0;
+  }
+  return true;
 }
 
 bool get_tip(int tip_number, int tip[TIP_SIZE])
 {
-    if (tip_number < 0 || tip_number >= get_line_numbers()){
+    if (tip_number < 0 || tip_number >= 44){
       return false;
     }
     char line[MAX_LINE_LEN];
@@ -82,28 +78,38 @@ bool set_drawing(int drawing_numbers[TIP_SIZE]){
   }
   return true;
 }
+
+bool is_empty(int drawing_numbers[])
+{
+    for (int i = 0; i < TIP_SIZE; ++i) {
+        if (drawing_numbers[i] != 0) return false;
+    }
+    return true;
+}
+
 int get_tip_result(int tip_number){
- return 0;
+  if (tip_number < 0 || tip_number >= 44) {
+    return -2;
+  }
+  if (is_empty(_last_drawing)){
+    return -1;
+  }
+  int tip[TIP_SIZE];
+  int count = 0;
+  get_tip(tip_number,tip);
+  for (int i = 0; i < TIP_SIZE; i++) {
+    for (int j = 0; j < TIP_SIZE; j++) {
+      if (_last_drawing[i] == tip[j]){
+        count++;
+      }
+    }
+  }
+  return count;
 }
 int get_right_tips_count(int right_digits_count){
  return 0;
 }
 
-int get_line_numbers()
-{
-  int previous = ftell(_stream);
-  rewind(_stream);
-  int amount_of_lines = 1;
-  while (!feof(_stream))
-  {
-      if (fgetc(_stream) == '\n')
-      {
-          amount_of_lines++;
-      }
-  }
-  fseek(_stream, previous, SEEK_SET);
-  return amount_of_lines;
-}
 
 
 bool contains_digit(int tip[TIP_SIZE], int digit)
